4784d5beae6955b0b400d35cc98b7361e32a4934
Matteo Vaccari <mvaccari@thoughtworks.com>
2025-09-24 14:38:17 +0200

    Add spring boot empty app

---
+++ spring-boot-pbw/src/main/java/it/xpug/pbw/PbwApplication.java
@@ -0,0 +1,13 @@
+package it.xpug.pbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbwApplication.class, args);
+	}
+
+}
---
+++ spring-boot-pbw/src/test/java/it/xpug/pbw/PbwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xpug.pbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
